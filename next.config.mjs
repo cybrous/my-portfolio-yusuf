b6a06737b3f03fa2c139e38730c9e0e31dca2212
Your Name <you@example.com>
2024-09-06 19:13:00 +0530

    updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -17,6 +17,19 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/(.*)', // Apply to all routes
+        headers: [
+          {
+            key: 'Cache-Control',
+            value: 'public, max-age=31536000, immutable', // Customize as needed
+          },
+        ],
+      },
+    ];
+  },
 };
 
 export default nextConfig;
