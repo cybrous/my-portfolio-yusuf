b313da2fe9e4e0d9bb2b0e1c6b94cdbc998a0e28
Your Name <you@example.com>
2024-09-06 20:37:39 +0530

    updated next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,10 +1,11 @@
 import { setupDevPlatform } from '@cloudflare/next-on-pages/next-dev';
 
-// Here we use the @cloudflare/next-on-pages next-dev module to allow us to use bindings during local development
-// (when running the application with `next dev`), for more information see:
-// https://github.com/cloudflare/next-on-pages/blob/main/internal-packages/next-dev/README.md
 if (process.env.NODE_ENV === 'development') {
-  await setupDevPlatform();
+  setupDevPlatform().then(() => {
+    console.log('Development platform setup complete.');
+  }).catch((error) => {
+    console.error('Error setting up development platform:', error);
+  });
 }
 
 /** @type {import('next').NextConfig} */
