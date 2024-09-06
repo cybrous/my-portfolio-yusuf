d0fe1cae7a689e542fe84f48a93be047d45fe385
Your Name <you@example.com>
2024-09-06 20:41:02 +0530

    updated intro.tsx for loading image

--- components/intro.tsx
+++ components/intro.tsx
@@ -38,6 +38,7 @@ export default function Intro() {
               quality={95}
               priority={true}
               className="h-24 w-24 rounded-full object-cover border-[0.35rem] border-white shadow-xl"
+              loading="eager"
             />
           </motion.div>
 
